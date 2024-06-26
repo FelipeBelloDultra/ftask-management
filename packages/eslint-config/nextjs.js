99c63d718a695b0cfac5ea775b855f2fa1665de9
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-06-25 21:23:57 -0300

    chore: add packages

---
+++ packages/eslint-config/nextjs.js
@@ -0,0 +1,71 @@
+/** @type {import('eslint').Linter.Config}  */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["standard", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["jsx-a11y", "@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "_",
+      },
+    ],
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "no-type-imports",
+        disallowTypeAnnotations: true,
+        fixStyle: "separate-type-imports",
+      },
+    ],
+    "sort-imports": [
+      "error",
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+      },
+    ],
+    "import/no-unresolved": "off",
+    "import/no-named-as-default-member": "off",
+    "import/no-named-as-default": 0,
+    "import/order": [
+      1,
+      {
+        groups: ["builtin", "external", "internal", "parent", "sibling", "index", "object", "type"],
+        "newlines-between": "always",
+      },
+    ],
+    "import/newline-after-import": "error",
+    "jsx-a11y/alt-text": [
+      "warn",
+      {
+        elements: ["img"],
+        img: ["Image"],
+      },
+    ],
+    "jsx-a11y/aria-props": "warn",
+    "jsx-a11y/aria-proptypes": "warn",
+    "jsx-a11y/aria-unsupported-elements": "warn",
+    "jsx-a11y/role-has-required-aria-props": "warn",
+    "jsx-a11y/role-supports-aria-props": "warn",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/parsers": {
+      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
+    },
+  },
+};
---
+++ packages/prettier/index.mjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+};
+
+export default config;
