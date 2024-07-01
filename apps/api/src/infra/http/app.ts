5c6b8e2d9b23f34b7f169c10277f1c634cbaa715
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-07-01 12:32:58 -0300

    feat(api/infra): create classes to start server

--- apps/api/src/infra/http/app.ts
+++ apps/api/src/infra/http/app.ts
@@ -1,9 +1,32 @@
 import express from "express";
 
+import { PrismaConnection } from "../database/prisma/prisma-connection";
+
 import { Routes } from "./routes";
 
-export const app = express();
-const { router } = new Routes();
+const expressInstance = express();
+const routes = new Routes();
+const prismaConnection = new PrismaConnection();
 
-app.use(express.json());
-app.use("/api", router);
+export class App {
+  public readonly expressInstance = expressInstance;
+  public readonly prismaConnection = prismaConnection;
+  public readonly routes = routes;
+
+  public async connectPrisma() {
+    return await prismaConnection.connect();
+  }
+
+  public async disconnectPrisma() {
+    return await prismaConnection.disconnect();
+  }
+
+  private registerRoutes() {
+    this.expressInstance.use("/api", this.routes.router);
+  }
+
+  public registerMiddlewares() {
+    this.expressInstance.use(express.json());
+    this.registerRoutes();
+  }
+}
--- apps/api/src/infra/http/server.ts
+++ apps/api/src/infra/http/server.ts
@@ -1,6 +1,37 @@
 import "dotenv/config";
+
 import { env } from "~/config/env";
 
-import { app } from "./app";
+import { App } from "./app";
 
-app.listen(env.HTTP_SERVER_PORT);
+const app = new App();
+
+class Server {
+  public async startServices() {
+    await app.connectPrisma();
+    app.registerMiddlewares();
+  }
+
+  public async stopServices() {
+    await app.disconnectPrisma();
+  }
+
+  public async boot() {
+    await this.startServices();
+
+    const server = app.expressInstance.listen(env.HTTP_SERVER_PORT);
+
+    process.on("SIGTERM", async () => {
+      await this.stopServices();
+      console.log("ops");
+
+      server.close();
+    });
+  }
+}
+
+const server = new Server();
+
+server.boot().catch(() => {
+  server.stopServices();
+});
