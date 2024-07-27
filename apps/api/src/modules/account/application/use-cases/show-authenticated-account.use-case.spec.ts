ebf15677c6029f23b85f40341ba11d5e6bcd203b
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-07-27 18:57:48 -0300

    feat(api/test): finish .todo use case tests

--- apps/api/src/modules/account/application/use-cases/show-authenticated-account.use-case.spec.ts
+++ apps/api/src/modules/account/application/use-cases/show-authenticated-account.use-case.spec.ts
@@ -1,1 +1,31 @@
-describe.todo("ShowAuthenticatedAccount");
+import { AccountNotFoundError } from "@/modules/project/application/use-cases/errors/account-not-found.error";
+import { makeAccount } from "@/test/factories/make-account";
+import { FakeAccountRepository } from "@/test/repositories/fake-account.repository";
+
+import { ShowAuthenticatedAccountUseCase } from "./show-authenticated-account.use-case";
+
+describe("ShowAuthenticatedAccount", () => {
+  let sut: ShowAuthenticatedAccountUseCase;
+  let fakeAccountRepository: FakeAccountRepository;
+
+  beforeEach(() => {
+    fakeAccountRepository = new FakeAccountRepository();
+    sut = new ShowAuthenticatedAccountUseCase(fakeAccountRepository);
+  });
+
+  it("should show the authenticated account", async () => {
+    const account = makeAccount();
+    await fakeAccountRepository.create(account);
+
+    const result = await sut.execute({ accountId: account.id.toValue() });
+
+    expect(result.isRight()).toBeTruthy();
+  });
+
+  it("should not show the authenticated account if account does not exists", async () => {
+    const result = await sut.execute({ accountId: "invalid-id" });
+
+    expect(result.isLeft()).toBeTruthy();
+    expect(result.value).toBeInstanceOf(AccountNotFoundError);
+  });
+});
--- apps/api/src/modules/notification/application/use-cases/fetch-notifications-by-recipient-id.use-cases.spec.ts
+++ apps/api/src/modules/notification/application/use-cases/fetch-notifications-by-recipient-id.use-cases.spec.ts
@@ -1,1 +1,44 @@
-describe.todo("FetchNotificationsByRecipientId");
+import { AccountNotFoundError } from "@/modules/project/application/use-cases/errors/account-not-found.error";
+import { makeAccount } from "@/test/factories/make-account";
+import { makeNotification } from "@/test/factories/make-notification";
+import { FakeAccountRepository } from "@/test/repositories/fake-account.repository";
+import { FakeNotificationRepository } from "@/test/repositories/fake-notification.repository";
+
+import { FetchNotificationsByRecipientIdUseCase } from "./fetch-notifications-by-recipient-id.use-cases";
+
+describe("FetchNotificationsByRecipientId", () => {
+  let sut: FetchNotificationsByRecipientIdUseCase;
+  let fakeNotificationRepository: FakeNotificationRepository;
+  let fakeAccountRepository: FakeAccountRepository;
+
+  beforeEach(() => {
+    fakeNotificationRepository = new FakeNotificationRepository();
+    fakeAccountRepository = new FakeAccountRepository();
+    sut = new FetchNotificationsByRecipientIdUseCase(fakeNotificationRepository, fakeAccountRepository);
+  });
+
+  it("should fetch notifications by recipient id", async () => {
+    const account = makeAccount();
+    await fakeAccountRepository.create(account);
+    const notifications = Array.from({ length: 10 }, () =>
+      makeNotification({
+        recipientId: account.id,
+      }),
+    );
+
+    await Promise.all(notifications.map((n) => fakeNotificationRepository.create(n)));
+
+    const result = await sut.execute({ recipientId: account.id.toValue() });
+
+    expect(result.isRight()).toBeTruthy();
+  });
+
+  it("should return an error if account not found", async () => {
+    const input = { recipientId: "non-existing-account-id" };
+
+    const result = await sut.execute(input);
+
+    expect(result.isLeft()).toBeTruthy();
+    expect(result.value).toBeInstanceOf(AccountNotFoundError);
+  });
+});
