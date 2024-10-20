30d387fcdff5f95f7c3f7cbcf43e97c22c2f2adf
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-10-20 14:18:35 -0300

    feat(api): create invite entity

---
+++ apps/api/src/modules/project/domain/entity/invite.ts
@@ -0,0 +1,57 @@
+import { Entity } from "@/core/entity/entity";
+import { UniqueEntityID } from "@/core/entity/unique-entity-id";
+import { Optional } from "@/core/types/optional";
+
+import { DueDate } from "./value-objects/due-date";
+import { InvitationStatus } from "./value-objects/invitation-status";
+
+export interface InviteProps {
+  expirationDate: DueDate;
+  memberId: UniqueEntityID;
+  projectId: UniqueEntityID;
+  status: InvitationStatus;
+  createdAt: Date;
+}
+
+export class Invite extends Entity<InviteProps> {
+  private static EXPIRATION_INVITE_TIME_IN_DAYS = 1;
+
+  public get expirationDate() {
+    return this.props.expirationDate;
+  }
+
+  public get memberId() {
+    return this.props.memberId;
+  }
+
+  public get projectId() {
+    return this.props.projectId;
+  }
+
+  public get status() {
+    return this.props.status;
+  }
+
+  public get createdAt() {
+    return this.props.createdAt;
+  }
+
+  private static setExpirationDate() {
+    const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + this.EXPIRATION_INVITE_TIME_IN_DAYS);
+
+    return currentDate;
+  }
+
+  public static create(props: Optional<InviteProps, "expirationDate" | "createdAt" | "status">, id?: UniqueEntityID) {
+    return new Invite(
+      {
+        ...props,
+        createdAt: props.createdAt || new Date(),
+        expirationDate: props.expirationDate || DueDate.create(Invite.setExpirationDate()),
+        status: props.status || InvitationStatus.create(),
+      },
+      id,
+    );
+  }
+}
