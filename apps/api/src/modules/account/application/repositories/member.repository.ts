26961b050bc874839b92d7589554633875fd89a5
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-11-08 17:08:08 -0300

    fix(api/member): remove member entity and repository interface

--- apps/api/src/modules/account/application/repositories/member.repository.ts
+++
@@ -1,9 +0,0 @@
-import { UniqueEntityID } from "@/core/entity/unique-entity-id";
-import { Member } from "@/modules/account/domain/entity/member";
-
-export interface MemberRepository {
-  create(member: Member): Promise<void>;
-  findById(id: UniqueEntityID): Promise<Member | null>;
-  findByAccountId(accountId: UniqueEntityID): Promise<Member | null>;
-  findByAccountAndProjectId(accountId: UniqueEntityID, projectId: UniqueEntityID): Promise<Member | null>;
-}
