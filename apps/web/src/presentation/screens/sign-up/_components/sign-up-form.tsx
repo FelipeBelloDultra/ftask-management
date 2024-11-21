7becf48f8174509bc8fbd3226c76ad3c94b0fea8
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-11-20 22:14:59 -0300

    feat(web): use input password in sign up form

--- apps/web/src/presentation/screens/sign-up/_components/sign-up-form.tsx
+++ apps/web/src/presentation/screens/sign-up/_components/sign-up-form.tsx
@@ -1,4 +1,5 @@
 import { Loader2Icon } from "@/presentation/components/icons";
+import { PasswordInput } from "@/presentation/components/input-password";
 import { Button } from "@/presentation/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/presentation/components/ui/form";
 import { Input } from "@/presentation/components/ui/input";
@@ -46,7 +47,7 @@ export function SignUpForm() {
             <FormItem>
               <FormLabel>Password</FormLabel>
               <FormControl>
-                <Input type="password" placeholder="my-password" className="w-full pr-11" {...field} />
+                <PasswordInput placeholder="my-password" {...field} />
               </FormControl>
               <FormMessage className="font-semibold" />
             </FormItem>
