4df87dc62def3e7de14e514bc22e9f0916b6d4e5
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-11-22 18:56:24 -0300

    feat(web/providers): use notification adapter in dependencies provider

--- apps/web/src/presentation/providers/dependencies-provider.tsx
+++ apps/web/src/presentation/providers/dependencies-provider.tsx
@@ -1,6 +1,7 @@
 import { createContext, ReactNode } from "react";
 
 import { AuthAdapter, AuthHttpAdapter } from "@/adapters/auth-adapter";
+import { NotificationAdapter, NotificationHttpAdapter } from "@/adapters/notification-adapter";
 import { ProfileAdapter, ProfileHttpAdapter } from "@/adapters/profile-adapter";
 import { HttpClientAdapter } from "@/infra/adapter/fetch-adapter-http";
 
@@ -11,6 +12,7 @@ interface DependenciesProviderProps {
 interface DependenciesContextProps {
   authAdapter: AuthAdapter;
   profileAdapter: ProfileAdapter;
+  notificationAdapter: NotificationAdapter;
 }
 
 export const DependenciesContext = createContext({} as DependenciesContextProps);
@@ -21,6 +23,7 @@ export function DependenciesProvider({ children }: DependenciesProviderProps) {
   const dependencies: DependenciesContextProps = {
     authAdapter: new AuthHttpAdapter(httpClient),
     profileAdapter: new ProfileHttpAdapter(httpClient),
+    notificationAdapter: new NotificationHttpAdapter(httpClient),
   };
 
   return <DependenciesContext.Provider value={dependencies}>{children}</DependenciesContext.Provider>;
