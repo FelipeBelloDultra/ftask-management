5a85f62042d13b5e05dc7084e7ae53e71153112b
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-11-22 17:31:29 -0300

    fix(web/services): remove unused showAuthenticatedService file and endpoint

--- apps/web/src/services/endpoints.ts
+++ apps/web/src/services/endpoints.ts
@@ -1,4 +1,3 @@
-export const SHOW_AUTHENTICATED_USER = "/account/session/me";
 export const REFRESH_TOKEN = "/account/session/refresh-token";
 export const COUNT_NOTIFICATIONS = "/notifications/count";
 export const ALL_NOTIFICATIONS = "/notifications";
