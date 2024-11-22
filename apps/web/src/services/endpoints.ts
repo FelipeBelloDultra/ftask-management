0b89960e18df2c67bf5439a89ddfc401745329d2
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-11-22 19:02:47 -0300

    fix(web/services): remove unused files and routes from notification

--- apps/web/src/services/endpoints.ts
+++ apps/web/src/services/endpoints.ts
@@ -1,6 +1,4 @@
 export const REFRESH_TOKEN = "/account/session/refresh-token";
-export const COUNT_NOTIFICATIONS = "/notifications/count";
 export const ALL_NOTIFICATIONS = "/notifications";
 export const NOTIFICATION_DETAIL = "/notifications/:notificationId";
-export const READ_NOTIFICATION = "/notifications/:notificationId/read";
 export const FETCH_INVOLVED_PROJECTS = "/projects";
