d82a350b41c092fe65ad44403d702396a4272569
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-11-23 23:41:59 -0300

    fix(web): remove nested route pages

--- apps/web/src/presentation/screens/index.ts
+++ apps/web/src/presentation/screens/index.ts
@@ -1,10 +1,10 @@
 import { lazy } from "react";
 
-export { default as NotificationDetailScreen } from "@/presentation/screens/notifications/details";
+export { default as NotificationDetailScreen } from "@/presentation/screens/notification-detail";
 
 export const SignInScreen = lazy(() => import("@/presentation/screens/sign-in"));
 export const SignUpScreen = lazy(() => import("@/presentation/screens/sign-up"));
 export const DashboardScreen = lazy(() => import("@/presentation/screens/dashboard"));
 export const NotificationsScreen = lazy(() => import("@/presentation/screens/notifications"));
 export const SettingsScreen = lazy(() => import("@/presentation/screens/settings"));
-export const SettingsProfileScreen = lazy(() => import("@/presentation/screens/settings/profile"));
+export const SettingsProfileScreen = lazy(() => import("@/presentation/screens/settings-profile"));
