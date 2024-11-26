b949d10891a0b5ae8387cd47a2cdfce229133562
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2024-11-26 18:52:52 -0300

    fix(api/project): add pagination and filters to findAllByMemberId

--- apps/api/src/modules/project/application/repositories/invite.repository.ts
+++ apps/api/src/modules/project/application/repositories/invite.repository.ts
@@ -1,11 +1,20 @@
+import { Pagination } from "@/core/entity/pagination";
 import { UniqueEntityID } from "@/core/entity/unique-entity-id";
 
 import { Invite } from "../../domain/entity/invite";
+
+export interface FindAllByMemberIdFilters {
+  status: "pending" | "accepted" | "declined";
+}
 
 export interface InviteRepository {
   findById(inviteId: UniqueEntityID): Promise<Invite | null>;
   save(invite: Invite): Promise<void>;
   create(invite: Invite): Promise<void>;
   findLastByMemberId(inviteId: UniqueEntityID): Promise<Invite | null>;
-  findAllByMemberId(memberId: UniqueEntityID): Promise<{ invites: Invite[]; total: number }>;
+  findAllByMemberId(
+    memberId: UniqueEntityID,
+    pagination: Pagination,
+    filters: FindAllByMemberIdFilters,
+  ): Promise<{ invites: Invite[]; total: number }>;
 }
